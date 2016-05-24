d051d660e4c40de3a96da0aad34aaec13f963cf5
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2016-05-24 14:32:39 +0200

    Implement HMAC ourselves

--- hash.c
+++ hash.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <openssl/hmac.h>
+#include <openssl/sha.h>
 
 void bin2hex(unsigned char *bin, unsigned int binsz, char * result)
 {
@@ -15,34 +16,60 @@ void bin2hex(unsigned char *bin, unsigned int binsz, char * result)
     }  
 }
 
+static inline void sha1_cpy(SHA_CTX *restrict out, const SHA_CTX *restrict in)
+{
+  out->h0 = in->h0;
+  out->h1 = in->h1;
+  out->h2 = in->h2;
+  out->h3 = in->h3;
+  out->h4 = in->h4;
+}
+
 void do_hash()
 {
     char salt[] = "ea911135";
     char pass[] = "pass1234";
-    
-    unsigned char digest[20];
-    int digest_len;
-    char mdString[40];
+
+    unsigned char digest[20] = {0};
+    int digest_len = sizeof(digest);
+    char mdString[41] = {0};
+
+    char padded_salt[64] = {0};
+    strcpy(padded_salt, salt);
+    char i_key_pad[64];
+    char o_key_pad[64];
+
+    for (int i = 0; i < 64; i++) {
+        i_key_pad[i] = 0x36 ^ padded_salt[i];
+    }
+
+    for (int i = 0; i < 64; i++) {
+        o_key_pad[i] = 0x5c ^ padded_salt[i];
+    }
+
+    SHA_CTX work_inner, work_outer;
+    SHA1_Init(&work_inner);
+    SHA1_Init(&work_outer);
 
     strcpy(mdString, pass);
 
-    HMAC_CTX orig, copy;
-    HMAC_CTX_init(&orig);
-    HMAC_CTX_init(&copy);
-
-    HMAC_Init_ex(&orig, salt, strlen(salt), EVP_sha1(), NULL);
-    HMAC_CTX_copy(&copy, &orig);
-
     for (int iter = 0; iter < 300000; iter++) {
-        EVP_MD_CTX_copy(&copy.md_ctx, &orig.md_ctx);
-        HMAC_Update(&copy, mdString, strlen(mdString));
-        HMAC_Final(&copy, digest, &digest_len);
+
+        SHA1_Init(&work_inner);
+        SHA1_Init(&work_outer);
+
+        SHA1_Update(&work_outer, o_key_pad, 64);
+        SHA1_Update(&work_inner, i_key_pad, 64);
+ 
+        SHA1_Update(&work_inner, mdString, strlen(mdString));
+        SHA1_Final(digest, &work_inner);
+
+        SHA1_Update(&work_outer, digest, 20);
+        SHA1_Final(digest, &work_outer);
 
         bin2hex(digest, digest_len, mdString);
     }
  
-    HMAC_CTX_cleanup(&copy);
-    HMAC_CTX_cleanup(&orig);
     printf("HMAC digest: %s\n", mdString);
     // should print f27f36023135a1dfb51517be5be0b421ee061d35
 }
